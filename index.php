f9c05ec4258ae970f02dd4f4e573efa8c07e39f7
unknown <softdc1998@gmail.com>
2023-12-10 19:37:13 +0530

    changes on index file

--- index.php
+++ index.php
@@ -4,17 +4,16 @@
     <div class="container overflow-hidden">  
         <div class="owl-carousel owl-theme default-carousel fables-sqr-nav dots-0 wow fadeInUpBig" data-wow-duration="2s">
               <div>
-                  <h1 class="white-color bold-font mt-lg-5 mb-4">CONSULTING SERVICE FOR <br> 
-                     <span class="fables-second-text-color">KIND OF BUSINESSES</span>
+                  <h1 class="white-color bold-font mt-lg-5 mb-4">Professional IT Development<br> 
                   </h1>  
                   <p class="fables-third-text-color mt-3 mb-5 light-font fables-header-slider-details">
-                      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                     We work at the intersection of creativity and technology. Building innovative products.
                   </p>
                   <a href="#" class="btn fables-second-background-color fables-second-border-color white-color rounded-0 mr-4 px-md-4 py-2 btn-bg-hover white-color-hover">Our Services</a>
                   <a href="#" class="btn fables-second-border-color white-color rounded-0 px-md-4 py-2 fables-second-hover-background-color">Learn More</a>  
               </div> 
               <div>
-                  <h1 class="white-color bold-font mt-lg-5 mb-4">CONSULTING SERVICE FOR ALL <br> 
+                  <h1 class="white-color bold-font mt-lg-5 mb-4">Software Development Done Right<br> 
                      <span class="fables-second-text-color">KIND OF BUSINESSES</span>
                   </h1>  
                   <p class="fables-third-text-color mt-3 mb-5 light-font fables-header-slider-details">
